f351ed6274861ab452d0fcebe3dd082f0b787596
theShahzadFarooq <crindalwalt1@gmail.com>
2022-05-30 12:54:48 +0500

    Course detail page
    lporem

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "components/_links.php" ?>
+    <title>Home Page</title>
+</head>
+
+<body>
+    <?php include "components/_navbar.php" ?>
+    <div class="container my-5">
+        <h1 class="my-3">Home Page</h1>
+    </div>
+</body>
+<?php include "components/_scripts.php" ?>
+
+</html>
